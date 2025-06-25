b3960e228acbc7b05f021ed1d421af2f6e6995dc
Emadeldeen Soliman <emade09@gmail.com>
2025-06-25 15:36:11 +0300

    refactor(bootstrap): routes for auth and api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,55 @@
 <?php
 
+use App\Http\Middleware\ApiLocalization;
+use App\Http\Controllers\BaseController;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware([ApiLocalization::class, 'web'])
+                ->group(base_path('routes/web.php'));
+
+            Route::middleware([ApiLocalization::class])
+                ->prefix('api/auth/')
+                ->group(base_path('routes/auth.php'));
+
+            Route::middleware([ApiLocalization::class])
+                ->prefix('api/')
+                ->group(base_path('routes/api.php'));
+
+
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        // UnAuth Error Handler
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->expectsJson()) {
+                $baseApiController = new BaseController();
+                return $baseApiController->sendResponse(false, [
+
+                ], "Unauthenticated", [], 401);
+            }
+        });
+
+        // Not Found Exception Error Handler
+        $exceptions->render(function (NotFoundHttpException  $e, Request $request) {
+            if ($request->expectsJson()) {
+                $baseApiController = new BaseController();
+                return $baseApiController->sendResponse(false, [
+
+                ], "EndPoint Is Not Found: " . $request->uri(), [], 404);
+            }
+        });
     })->create();
---
+++ routes/auth.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\v1\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/* ================================ Start:: Public Auth Routes ================================ */
+Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('register', [AuthController::class, 'register'])->name('auth.register');
+/* ================================ End:: Public Auth Routes ================================ */
+
+/* ================================ Start:: Protected Auth Routes ================================ */
+Route::middleware('auth:sanctum')->group(function () {
+    // Routes for all authenticated users
+    Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout'); 
+});
+/* ================================ End:: Protected Auth Routes ================================ */
