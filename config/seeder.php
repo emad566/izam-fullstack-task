bce7ef134129f0502c213fbe8fb1b1b6325c2e29
Emadeldeen Soliman <emade09@gmail.com>
2025-06-25 16:00:40 +0300

    chore(config): config seeder data

---
+++ config/seeder.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'users' => [
+        'admin' => [
+            'name' => 'Admin',
+            'email' => 'emade09@gmail.com',
+            'password' => '12345678',
+        ],
+    ],
+];
