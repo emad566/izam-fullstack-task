c2217a8a79e810154951b8f0499696f9b246ad02
Emadeldeen Soliman <emade09@gmail.com>
2025-06-25 16:01:32 +0300

    feat(Seed): first admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->createAdmin();
+    }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    private function createAdmin()
+    {
+        $admin = config('seeder.users.admin');
+        User::create($admin);
     }
 }
