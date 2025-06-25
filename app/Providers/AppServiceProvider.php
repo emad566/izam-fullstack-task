5f4954d4ce1337b3b0ffc622b8649b1305637dd7
Emadeldeen Soliman <emade09@gmail.com>
2025-06-25 15:00:46 +0300

    feat(AppServiceProvider): boot macros

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,45 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading(true);
+        Schema::defaultStringLength(191);
+
+        URL::forceScheme('https');
+
+        Builder::macro('betweenEqual', function ($field, $array) {
+            return $this->where($field, '>=', $array[0])
+                ->where($field, '<=', $array[1]);
+        });
+
+        Builder::macro('like', function ($field, $string) {
+            return $string ? $this->where($field, 'like', '%' . $string . '%') : $this;
+        });
+
+        Builder::macro('likeEnd', function ($field, $string) {
+            return $string ? $this->where($field, 'like', '%' . $string) : $this;
+        });
+
+        Builder::macro('likeStart', function ($field, $string) {
+            return $string ? $this->where($field,  'like', $string . '%') : $this;
+        });
+        Builder::macro('orLikeEnd', function ($field, $string) {
+            return $string ? $this->orWhere($field, 'like', '%' . $string) : $this;
+        });
+
+        Builder::macro('orLikeStart', function ($field, $string) {
+            return $string ? $this->orWhere($field,  'like', $string . '%') : $this;
+        });
+
+        Builder::macro('orLike', function ($field, $string) {
+            return $string ? $this->orWhere($field, 'like', '%' . $string . '%') : $this;
+        });
+
+        Builder::macro('active', function () {
+            return $this->whereNull('deleted_at');
+        });
+
+        Builder::macro('inActive', function () {
+            return $this->whereNotNull('deleted_at');
+        });
     }
 }
