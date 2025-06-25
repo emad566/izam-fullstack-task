61f15011e2b8e851d0f253283b1a0b98d23b4aee
Emadeldeen Soliman <emade09@gmail.com>
2025-06-25 16:45:48 +0300

    refactor(AuthController): to UserAuthController

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
-class AuthController extends BaseController
+class UserAuthController extends BaseController
 {
     /**
      * Register a new user.
--- routes/auth.php
+++ routes/auth.php
@@ -1,16 +1,16 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserAuthController;
 use Illuminate\Support\Facades\Route;
 
 /* ================================ Start:: Public Auth Routes ================================ */
-Route::post('login', [AuthController::class, 'login'])->name('auth.login');
-Route::post('register', [AuthController::class, 'register'])->name('auth.register');
+Route::post('login', [UserAuthController::class, 'login'])->name('auth.login');
+Route::post('register', [UserAuthController::class, 'register'])->name('auth.register');
 /* ================================ End:: Public Auth Routes ================================ */
 
 /* ================================ Start:: Protected Auth Routes ================================ */
 Route::middleware('auth:sanctum')->group(function () {
     // Routes for all authenticated users
-    Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('logout', [UserAuthController::class, 'logout'])->name('auth.logout');
 });
 /* ================================ End:: Protected Auth Routes ================================ */
