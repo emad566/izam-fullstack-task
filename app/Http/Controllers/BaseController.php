7f4d54410a40ba861ed885f619599c10d84e1ea4
Emadeldeen Soliman <emade09@gmail.com>
2025-06-25 14:54:05 +0300

    feat(BaseController):

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,76 @@
+<?php
+
+
+namespace App\Http\Controllers\v1;
+
+use App\Http\Controllers\Controller;
+
+class BaseController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+     protected string $table = '';
+     protected string $primaryKey = '';
+     protected ?string $model = null;
+     protected string $resource;
+     protected ?string $modelRequest = null;
+     protected $data = [];
+
+     protected array $excludedColumns = [];
+
+     protected array $columns = [];
+     protected array $heading = [];
+
+     // Allowing Default Argument
+     public function __construct(?string $model = null, array $excludedColumns = [])
+     {
+         if ($model) {
+             $this->model = $model;
+             $this->excludedColumns = $excludedColumns;
+             $modelInstance = new $this->model();
+             $this->columns = $modelInstance->getFillable();
+
+              if (!empty($this->excludedColumns)) {
+                 $this->columns = array_filter($this->columns, function($column) {
+                     return !in_array($column, $this->excludedColumns);
+                 });
+                 $this->columns = array_values($this->columns);
+             }
+
+             if($this->columns['date_from']?? false) unset($this->columns['date_from']);
+             if($this->columns['date_to']?? false) unset($this->columns['date_to']);
+             $this->primaryKey = $modelInstance->getKeyName();
+             $this->table = $modelInstance->getTable();
+             $modelResource = class_basename($modelInstance) . 'Resource';
+             $modelRequest = class_basename($modelInstance) . 'Request';
+             $this->resource = "App\Http\Resources\\$modelResource";
+             $this->modelRequest = "App\Http\Requests\\$modelRequest";
+         }
+     }
+
+    public function sendResponse($status = true, $data = null, $message = '', $errors = null, $code = 200, $request = null)
+    {
+        $response = [
+            'status' =>  $status,
+            'message' => $message,
+            'data'    => $data,
+            'errors' => $status === true ? $errors : (count($errors ?? [], COUNT_RECURSIVE) > 1 ? $errors : ['message' => [$message]]),
+
+            // TODO:: comment this lines
+            'response_code' => $code,
+            'request_data' => $request?->all(),
+        ];
+        return response()->json($response, $code);
+    }
+
+    public function sendServerError($msg = '', $data = null, $th = false)
+    {
+        $thStr = $th ? $th->getMessage() : '';
+        return $this->sendResponse(false, $data, 'Server Technical Error: ' . $msg . " $thStr", null, 500);
+    }
+
+}
