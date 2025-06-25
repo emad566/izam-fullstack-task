ed679de54957e36d2242bfcc57d1d6d41d31cb17
Emadeldeen Soliman <emade09@gmail.com>
2025-06-25 15:09:54 +0300

    chore(lang): installation

---
+++ lang/ar/msg.php
@@ -0,0 +1,23 @@
+<?php
+
+return array (
+  'can-not-delete-coupon-having-payment' => 'لا يمكن حذف القسيمة التي لديها دفعة',
+  'can-not-delete-video-having-certificates' => 'لا يمكن حذف الفيديو الذي يحتوي على شهادة!',
+  'can-not-delete-video-having-questions' => 'لا يمكن حذف الفيديو الذي يحتوي على سؤال',
+  'can-not-delete-video-having-scenes' => 'لا يمكن حذف الفيديو الذي يحتوي على مشاهد',
+  'can-not-edte-coupon-having-payment' => 'لا يمكن تحرير قسيمة مرتبطه بعمليه دفع',
+  'data-to-create' => 'البيانات لإنشاء',
+  'hasNotCertificate' => 'لم يشترك في برامج',
+  'noCoupon' => 'لا قسيمة',
+  'noName' => 'لا اسم',
+  'technicalError' => 'خطأ فني',
+  'created' => 'تم إنشاء',
+  'updated' => 'تم تحديث',
+  'deleted' => 'تم حذف',
+  'not-found' => 'لم يتم العثور على',
+  'not-found-item' => 'لم يتم العثور على العنصر',
+  'not-found-item-with-id' => 'لم يتم العثور على العنصر بالمعرف',
+  'not-found-item-with-id-and-trashed' => 'لم يتم العثور على العنصر بالمعرف والمحذوف',
+  'success' => 'تم العملية بنجاح',
+  'sync-categories' => 'تم جلب الفئات بنجاح',
+);
