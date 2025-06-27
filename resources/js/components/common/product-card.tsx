e7f464c4afc4703d741bc7f9ce95ad555000fc8f
Emadeldeen Soliman <emade09@gmail.com>
2025-06-27 14:16:45 +0300

    feat(react): product-> add sidebar and product card

--- resources/js/components/common/product-card.tsx
+++ resources/js/components/common/product-card.tsx
@@ -2,31 +2,48 @@ import type { Product } from "@/pages/@types"
 import { Card, CardContent } from "../ui/card"
 import { Badge } from "../ui/badge"
 import ProductCountButton from "./product-count-button"
+import { SideModal } from "./drawer"
+import { useModal } from "@/contexts/modal-context"
 
 const ProductCard = (props: Product) => {
+  const { openModal } = useModal()
   return (
-    <Card className="border-0 pt-0">
-      <div className="w-full aspect-[9/10]">
-        <img
-          src={props.image_urls}
-          alt={props.name}
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <CardContent className="space-y-3">
-        <div className="flex gap-4 flex-nowrap">
-          <p className="font-bold w-full truncate">{props.name}</p>
-          <Badge variant="secondary">{props.category.name}</Badge>
+    <>
+      <Card className="border-0 pt-0">
+        <div className="w-full aspect-[9/10]">
+          <img
+            src={props.image_urls}
+            alt={props.name}
+            className="w-full h-full object-cover"
+          />
         </div>
-        <div className="flex gap-4 justify-between">
-          <p className="font-bold">${props.price}</p>
-          <span className="text-[#6B7280] text-sm">Stock : {props.stock}</span>
-        </div>
-        <div>
-          <ProductCountButton id={props.id} />
-        </div>
-      </CardContent>
-    </Card>
+        <CardContent className="space-y-3">
+          <div className="flex gap-4 flex-nowrap">
+            <p
+              onClick={() => {
+                openModal(`product-${props.id}`)
+              }}
+              className="font-bold w-full truncate"
+            >
+              {props.name}
+            </p>
+            <Badge variant="secondary">{props.category.name}</Badge>
+          </div>
+          <div className="flex gap-4 justify-between">
+            <p className="font-bold">${props.price}</p>
+            <span className="text-[#6B7280] text-sm">
+              Stock : {props.stock}
+            </span>
+          </div>
+          <div>
+            <ProductCountButton id={props.id} />
+          </div>
+        </CardContent>
+      </Card>
+      <SideModal id={`product-${props.id}`}>
+        <div>test</div>
+      </SideModal>
+    </>
   )
 }
 
